63812bf938a552b82522e7a7139665d05f4f5803
Jonas Praem <jonaspraem@gmail.com>
2017-09-11 22:18:34 +0200

    Adding account-panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ProfileComponent } from './components/profile/profile/profile.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AccountPanelComponent } from './components/account-panel/account-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ProfileComponent
+    ProfileComponent,
+    AccountPanelComponent
   ],
   imports: [
     BrowserModule
