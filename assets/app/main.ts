fc1bee9c90ba086b38425370b076108e1e16bd20
Jonas Praem <jonaspraem@gmail.com>
2017-09-23 03:19:19 +0200

    Introducing Node.js, Express and webpack to stack

--- assets/app/main.ts
+++ assets/app/main.ts
@@ -1,14 +1,18 @@
+import './polyfills.ts';
+import 'zone.js';
+import 'reflect-metadata';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 // Google material design
 import 'hammerjs';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './web-app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
