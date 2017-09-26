4ea72e091b6e80cdca9977220733dce258642454
Jonas Praem <jonaspraem@gmail.com>
2017-09-26 17:30:32 +0200

    Configuring Angular Front-end setup

--- assets/app/web-app/app.module.ts
+++ assets/app/web-app/app.module.ts
@@ -1,27 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
-// Google material design
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
-
+//Components
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AccountPanelComponent } from './components/account-panel/account-panel.component';
+import { MessageComponent } from "../messages/message.component";
+
+// Google material design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     ProfileComponent,
-    AccountPanelComponent
+    AccountPanelComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule,
-    MdCheckboxModule
+    MdCheckboxModule,
+    FormsModule
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
