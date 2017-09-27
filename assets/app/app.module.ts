d35ddf77b122963043c1de2ff36e0ae6169be06f
Jonas Praem <jonaspraem@gmail.com>
2017-09-27 15:21:23 +0200

    Adding front-end routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -14,26 +14,34 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 import {MessageListComponent} from "./messages/message-list.component";
 import {MessageInputComponent} from "./messages/message-input.component";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
+import {HeaderComponent} from "./header.component";
+import {routing} from "./app.routing";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    ProfileComponent,
-    AccountPanelComponent,
-    MessageComponent,
-    MessageListComponent,
-    MessageInputComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MdButtonModule,
-    MdCheckboxModule,
-    FormsModule
-  ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SidebarComponent,
+        ProfileComponent,
+        AccountPanelComponent,
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdCheckboxModule,
+        FormsModule,
+        routing
+    ],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
