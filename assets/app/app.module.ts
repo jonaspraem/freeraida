a2efeac623a349c8dd15396d3e6a38454fe69da5
Jonas Praem <jonaspraem@gmail.com>
2017-10-03 17:53:47 +0200

    Adding lazyloading by using sub-modules

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,25 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AccountPanelComponent } from './account-panel/account-panel.component';
-import { MessageComponent } from "./messages/message.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MessageListComponent } from "./messages/message-list.component";
-import { MessageInputComponent } from "./messages/message-input.component";
-import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
 import { routing } from "./app.routing";
-import { LogoutComponent } from "./auth/logout.component";
-import { SignInComponent } from "./auth/sign-in.component";
-import { SignUpComponent } from "./auth/sign-up.component";
 import { HttpModule } from "@angular/http";
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
+import { MessageModule } from "./messages/message.module";
 
 @NgModule({
     declarations: [
@@ -27,24 +20,16 @@ import { ErrorService } from "./errors/error.service";
         SidebarComponent,
         ProfileComponent,
         AccountPanelComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SignInComponent,
-        SignUpComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        FormsModule,
         routing,
-        ReactiveFormsModule,
-        HttpModule
+        HttpModule,
+        MessageModule
     ],
     schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     providers: [AuthService, ErrorService],
--- assets/app/auth/auth.routing.ts
+++ assets/app/auth/auth.routing.ts
@@ -1,11 +1,13 @@
-import {Routes} from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import {SignUpComponent} from "./sign-up.component";
 import {SignInComponent} from "./sign-in.component";
 import {LogoutComponent} from "./logout.component";
 
-export const AUTH_ROUTES: Routes = [
+const AUTH_ROUTES: Routes = [
     { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
     { path: 'sign-up', component: SignUpComponent },
     { path: 'sign-in', component: SignInComponent },
     { path: 'logout', component: LogoutComponent },
 ];
+
+export const authRouting = RouterModule.forChild(AUTH_ROUTES);
