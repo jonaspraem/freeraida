a9eda1e1e00eb00fa1eb3766ce5422f0bb9813bc
Jonas Praem <jonaspraem@gmail.com>
2017-10-17 04:22:16 +0200

    Changing styling of post header

--- assets/app/posts/post.component.ts
+++ assets/app/posts/post.component.ts
@@ -13,6 +13,43 @@ export class PostComponent {
 
     constructor(private postService : PostService) {}
 
+    getFormattedDate() {
+        if (this.post.timestamp) {
+            var timestamp: Date = this.post.timestamp;
+            var date = timestamp.getDate();
+            var month_index = timestamp.getMonth();
+            var month;
+            switch (month_index) {
+                case 0:
+                    month = 'Jan'; break;
+                case 1:
+                    month = 'Feb'; break;
+                case 2:
+                    month = 'Mar'; break;
+                case 3:
+                    month = 'Apr'; break;
+                case 4:
+                    month = 'May'; break;
+                case 5:
+                    month = 'Jun'; break;
+                case 6:
+                    month = 'Jul'; break;
+                case 7:
+                    month = 'Aug'; break;
+                case 8:
+                    month = 'Sep'; break;
+                case 9:
+                    month = 'Oct'; break;
+                case 10:
+                    month = 'Nov'; break;
+                case 11:
+                    month = 'Dec'; break;
+            }
+            return date + ' ' + month;
+        }
+        return '';
+    }
+
     onEdit() {
         this.postService.editPost(this.post);
     }
