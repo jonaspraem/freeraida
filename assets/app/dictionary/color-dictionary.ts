d05514402d1037e1097fcad5f26b48b5cfa65c15
Jonas Praem <jonaspraem@gmail.com>
2018-01-15 18:48:22 +0100

    Adding line user input information to component

--- assets/app/dictionary/color-dictionary.ts
+++ assets/app/dictionary/color-dictionary.ts
@@ -12,6 +12,7 @@ export class COLOR_DICTIONARY {
         COLOR_DICTIONARY.COLOR_MAP.set('primary', '#141D2F');
         COLOR_DICTIONARY.COLOR_MAP.set('secondary', '#6495ED');
         COLOR_DICTIONARY.COLOR_MAP.set('black', '#000000');
+        COLOR_DICTIONARY.COLOR_MAP.set('white', '#FFFFFF');
         COLOR_DICTIONARY.COLOR_MAP.set('secondary_light', '#C1D4F7');
         COLOR_DICTIONARY.COLOR_MAP.set('ascent', '#448BDD');
         COLOR_DICTIONARY.COLOR_MAP.set('descent', '#560000');
@@ -19,6 +20,12 @@ export class COLOR_DICTIONARY {
         COLOR_DICTIONARY.COLOR_MAP.set('address', '#42BCF1');
         COLOR_DICTIONARY.COLOR_MAP.set('available', '#12E112');
         COLOR_DICTIONARY.COLOR_MAP.set('not-available', '#F60C00');
+        // DANGER LEVELS
+        COLOR_DICTIONARY.COLOR_MAP.set('Safe', '#08DB72');
+        COLOR_DICTIONARY.COLOR_MAP.set('Caution', '#F6EC01');
+        COLOR_DICTIONARY.COLOR_MAP.set('Danger', '#D00202');
+        COLOR_DICTIONARY.COLOR_MAP.set('Extreme', '#F69D3A');
+
     }
 
     get(key: string) {
