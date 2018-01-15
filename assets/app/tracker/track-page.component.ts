1060a5be6a8c3bf2b3f52831d26793c71407e75c
Jonas Praem <jonaspraem@gmail.com>
2018-01-15 23:46:21 +0100

    Adding timer to track component

--- assets/app/tracker/track-page.component.ts
+++ assets/app/tracker/track-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 import { COLOR_DICTIONARY } from "../dictionary/color-dictionary";
-import { Observable } from "rxjs/Observable";
 import { UnregisteredLine } from "../objects/models/unregistered-line.model";
 import { LineLocation } from "../objects/models/line-location.model";
+import { Observable } from "rxjs/Observable";
+import { TimerObservable } from "rxjs/observable/TimerObservable";
+import { Subscription } from "rxjs/Subscription";
 
 // Images
 const ascent = require('../../images/track-interface/mountaineering.jpg');
@@ -23,36 +25,54 @@ export class TrackPageComponent {
     public track_header = track_header;
 
     private location = {};
-    private ticker;
+    private subscription_ticker: Subscription;
+    private subscription_timer: Subscription;
     private isTracking;
     private tracked_line: UnregisteredLine = new UnregisteredLine([]);
     private ticks = 0;
+    private duration: string = '0:00';
 
     constructor(public color_dictionary: COLOR_DICTIONARY) {}
 
     startTracking() {
         this.isTracking = true;
 
-        this.ticker= Observable.timer(5000);
-        this.ticker.subscribe((t) => this.onTimeOut(t));
+        let ticker = TimerObservable.create(5000, 5000);
+        this.subscription_ticker = ticker.subscribe((t) => this.onTimeOut(t));
 
-        let timer = Observable.timer(1000);
-        timer.subscribe(t=> this.ticks = t);
+        let timer = TimerObservable.create(1000, 1000);
+        this.subscription_timer = timer.subscribe(t => {
+            this.ticks = t;
+            this.duration = this.getTime();
+        });
     }
 
     stopTracking() {
         this.isTracking = false;
-        this.ticker = '';
+        this.subscription_ticker.unsubscribe();
+        this.subscription_timer.unsubscribe();
     }
 
     onTimeOut(data) {
-        console.log(data);
         navigator.geolocation.getCurrentPosition(position => {
             console.log(position);
             this.location = position.coords;
             // in your case
             this.tracked_line.locations.push(new LineLocation(this.ticks, position.coords.latitude, position.coords.longitude))
         });
+    }
+
+    getTime() {
+        let time = this.ticks;
+        let sec_display: string;
+        let min_display: string;
+        let min = time/60;
+        let sec = time%60;
+        if (sec < 10) sec_display = "0" + sec.toString().substring(0, 1);
+        else sec_display = sec.toString();
+        if (min.toString().indexOf('.') > 0) min_display = min.toString().substring(0, min.toString().indexOf('.'));
+        else min_display = min.toString();
+        return min_display + ':' + sec_display;
     }
 
 
