15e93d705384af9d8c24fa364b1b7467de7d48ec
Jonas Praem <jonaspraem@gmail.com>
2018-01-19 20:36:19 +0100

    Adding deletion of tracked lines

--- models/schemas/tracked-line.js
+++ models/schemas/tracked-line.js
@@ -1,19 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var Profile = require('./profile');
+var Location = require('./location');
 
 var schema = new Schema({
     user_id: {type: String, required: true},
     locations: [{type: Schema.Types.ObjectId, ref: 'Location'}]
 });
 
-schema.post('remove', function(line) {
-    console.log('after delete');
-    Profile.findOne({user_id: line.user_id}, function(err, profile) {
-        console.log('profile '+profile);
-        profile.tracked_lines.pull(line._id);
+schema.pre('remove', function(next) {
+    var model = this;
+    var profile = require('./profile');
+    Location.find({_id: {$in: model.locations}}, function(err, result) {
+        result.forEach(function(location) {
+            location.remove(function(err) {});
+        });
+    });
+    profile.findOne({user_id: model.user_id}, function(err, profile) {
+        profile.tracked_lines.pull(model._id);
         profile.save();
+        next();
     });
 });
 
