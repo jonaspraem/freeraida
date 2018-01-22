344fc29cc94df369b9b260dcffae312fac371dde
Jonas Praem <jonaspraem@gmail.com>
2018-01-22 16:27:52 +0100

    Adding another redirect

--- assets/app/webapp.routing.ts
+++ assets/app/webapp.routing.ts
@@ -6,12 +6,14 @@ import { TrackPageComponent } from "./tracker/track.component";
 import { SettingsComponent } from "./profile/settings/settings.component";
 import { RegisterLineComponent } from "./lines/register-line.component";
 import { RidesComponent } from "./rides/rides.component";
+import { ForumsComponent } from "./coming-soon/forums/forums.component";
 
 const WEBAPP_ROUTES: Routes = [
     { path: 'feed', component: LiveFeedComponent},
     { path: 'register-line', component: RegisterLineComponent},
     { path: 'rides', component: RidesComponent},
     { path: 'track', component: TrackPageComponent},
+    { path: 'forums', component: ForumsComponent},
     { path: 'settings', component: SettingsComponent},
     { path: 'user/:id', component: ProfileComponent}
 ];
