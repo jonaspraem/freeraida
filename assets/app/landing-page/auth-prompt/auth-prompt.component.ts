85bf555b38548d23b92fa3f5061e588271988255
Jonas Praem <jonaspraem@gmail.com>
2018-04-22 22:56:36 +0200

    Adding animations

--- assets/app/landing-page/auth-prompt/auth-prompt.component.ts
+++ assets/app/landing-page/auth-prompt/auth-prompt.component.ts
@@ -1,15 +1,22 @@
 import { Component } from "@angular/core";
+import { Animations } from "../../@shared/animations/animations";
 
 const logoImage = require('../../../images/logo/favicon_kickstarter.png');
 
 @Component({
     selector: 'app-auth-prompt',
     templateUrl: './auth-prompt.component.html',
-    styleUrls: ['./auth-prompt.component.css']
+    styleUrls: ['./auth-prompt.component.css'],
+    animations: [Animations.slideUpDown]
 })
 
 export class AuthPromptComponent {
     public logo = logoImage;
     public enlistText = 'ENLIST NOW';
+    public isEnlistFormOpen = false;
+
+    onEnlist() {
+        this.isEnlistFormOpen = true;
+    }
 
 }
