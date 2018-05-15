872ab4b0bf51b801406076d606b7c8f8509f1444
Jonas Praem <jonaspraem@gmail.com>
2018-05-15 17:57:56 +0200

    Restyling landing page

--- assets/app/landing-page/landing-page.component.ts
+++ assets/app/landing-page/landing-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+
 const image = require('../../images/350214-mountain-view.jpg');
+
+const logoImage = require('../../images/logo/favicon_kickstarter.png');
+const backgroundImage = require('../../images/backgrounds/snow-3193865_1920.jpg');
 
 @Component({
     selector: 'app-landing-page',
@@ -8,7 +12,9 @@ const image = require('../../images/350214-mountain-view.jpg');
     styleUrls: ['./landing-page.component.css']
 })
 
-export class LandingPageComponent implements OnInit{
+export class LandingPageComponent implements OnInit {
+    public logo = logoImage;
+    public background = backgroundImage;
 
     constructor(private router: Router) {}
 
