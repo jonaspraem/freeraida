f00158cf800920645f890005286a6787dc319736
Jonas Praëm Andreassen <jpa@impact.dk>
2018-09-13 15:49:54 +0200

    Add config keys

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '372461699921-uv13me7jddkijchdsll7ttppmu8m5pjq.apps.googleusercontent.com',
+        clientSecret: 'wvtfYHc4dqJ02Y3CPXf1pC5q'
+    },
+    session: {
+        sessionKey: '67543hgrjh?=(qwplzamkfg21$333++874t23',
+        cookieKey: 'poewurvhgsajgnshcuv675467465654?dsabhgrmkgt4565442lkewq'
+    }
+};
