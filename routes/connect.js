3e8a0bfb7b5300fa045bfdc8afe5c3568f06e7ec
Jonas Praëm <jpa@gmail.com>
2018-09-21 18:19:10 +0100

    ES6 Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
-var Profile = require('../models/schemas/profile');
+const Profile = require('../models/schemas/profile');
 
 // Verify token
-router.use('/', function(req, res, next) {
+router.use('/', (req, res, next) => {
     request.post(
         'https://freeraida.eu.auth0.com/tokeninfo',
         { json: { id_token: req.query.token } },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 next();
             } else {
                 console.log(response);
@@ -24,13 +24,13 @@ router.use('/', function(req, res, next) {
 });
 
 // Follow another user
-router.post('/follow/:user_address', function(req, res, next) {
+router.post('/follow/:user_address', (req, res, next) => {
     request.post(
         'https://freeraida.eu.auth0.com/tokeninfo',
         { json: { id_token: req.query.token } },
-        function (error, response, body) {
+        (error, response, body) => {
             if (!error) {
-                Profile.findOne({user_id: body.user_id}, function(profile_err, own_profile) {
+                Profile.findOne({user_id: body.user_id}, (profile_err, own_profile) => {
                     if (profile_err) {
                         return res.status(500).json({
                             title: 'An error occured',
@@ -44,14 +44,14 @@ router.post('/follow/:user_address', function(req, res, next) {
                         });
                     }
 
-                    var isAlreadyFollowing = (own_profile.following.indexOf(req.params.user_address) > -1);
+                    const isAlreadyFollowing = (own_profile.following.indexOf(req.params.user_address) > -1);
                     if (isAlreadyFollowing) {
                         return res.status(500).json({
                             title: 'User is already followed',
                             error: {message: 'You are already following this user'}
                         });
                     }
-                    Profile.findOne({user_address: req.params.user_address}, function(p_err, profile_toFollow) {
+                    Profile.findOne({user_address: req.params.user_address}, (p_err, profile_toFollow) => {
                         if (p_err) {
                             return res.status(500).json({
                                 title: 'An error occured',
@@ -66,14 +66,14 @@ router.post('/follow/:user_address', function(req, res, next) {
                         }
                         own_profile.following.push(req.params.user_address);
                         profile_toFollow.followers.push(own_profile.user_address);
-                        own_profile.save(function (err, result_ownProfile) {
+                        own_profile.save((err, result_ownProfile) => {
                             if (err) {
                                 return res.status(500).json({
                                     title: 'An error occured',
                                     error: {message: 'An error occured'}
                                 });
                             }
-                            profile_toFollow.save(function (err, result) {
+                            profile_toFollow.save((err, result) => {
                                 if (err) {
                                     return res.status(500).json({
                                         title: 'An error occured',
@@ -93,13 +93,13 @@ router.post('/follow/:user_address', function(req, res, next) {
 });
 
 // Un-follow user
-router.post('/unfollow/:user_address', function(req, res, next) {
+router.post('/unfollow/:user_address', (req, res, next) => {
     request.post(
         'https://freeraida.eu.auth0.com/tokeninfo',
         { json: { id_token: req.query.token } },
-        function (error, response, body) {
+        (error, response, body) => {
             if (!error) {
-                Profile.findOne({user_id: body.user_id}, function(profile_err, own_profile) {
+                Profile.findOne({user_id: body.user_id}, (profile_err, own_profile) => {
                     if (profile_err) {
                         return res.status(500).json({
                             title: 'An error occured',
@@ -112,14 +112,14 @@ router.post('/unfollow/:user_address', function(req, res, next) {
                             error: {message: 'No user found'}
                         });
                     }
-                    var isAlreadyFollowing = (own_profile.following.indexOf(req.params.user_address) > -1);
+                    const isAlreadyFollowing = (own_profile.following.indexOf(req.params.user_address) > -1);
                     if (!isAlreadyFollowing) {
                         return res.status(500).json({
                             title: 'User not followed',
                             error: {message: 'User is not being followed by you'}
                         });
                     }
-                    Profile.findOne({user_address: req.params.user_address}, function(p_err, profile_toUnfollow) {
+                    Profile.findOne({user_address: req.params.user_address}, (p_err, profile_toUnfollow) => {
                         if (p_err) {
                             return res.status(500).json({
                                 title: 'An error occured',
@@ -134,14 +134,14 @@ router.post('/unfollow/:user_address', function(req, res, next) {
                         }
                         own_profile.following.splice(own_profile.following.indexOf(req.params.user_address), 1);
                         profile_toUnfollow.followers.splice(profile_toUnfollow.followers.indexOf(own_profile.user_address), 1);
-                        own_profile.save(function (err, result_ownProfile) {
+                        own_profile.save((err, result_ownProfile) => {
                             if (err) {
                                 return res.status(500).json({
                                     title: 'An error occured',
                                     error: {message: 'An error occured'}
                                 });
                             }
-                            profile_toUnfollow.save(function (err, result) {
+                            profile_toUnfollow.save((err, result) => {
                                 if (err) {
                                     return res.status(500).json({
                                         title: 'An error occured',
