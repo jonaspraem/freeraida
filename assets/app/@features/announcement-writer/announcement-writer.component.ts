1bc721fb2f397d0bbf3e81ac93d7f040c5ba6d00
Jonas Praem <jonaspraem@gmail.com>
2018-10-21 16:41:15 +0200

    Impl announcement writer

--- assets/app/@features/announcement-writer/announcement-writer.component.ts
+++ assets/app/@features/announcement-writer/announcement-writer.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { faFeatherAlt} from "@fortawesome/free-solid-svg-icons";
+
+const default_image = require('../../../images/rider/default-image.png');
 
 @Component({
     selector: 'app-announcement-writer',
@@ -7,6 +9,14 @@ import { faFeatherAlt} from "@fortawesome/free-solid-svg-icons";
 })
 
 export class AnnouncementWriterComponent {
+    @ViewChild('writerContainer') writerContainer: ElementRef;
+    @ViewChild('writer') writer: ElementRef;
+    public defaultImage = default_image;
     public icon = faFeatherAlt;
+
+    public expand(): void {
+        this.writerContainer.nativeElement.className = 'announcement-writer announcement-writer_open';
+        this.writer.nativeElement.className = 'announcement-writer__textwriter announcement-writer__textwriter_open';
+    }
 
 }
