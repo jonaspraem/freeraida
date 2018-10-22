4737351e7b8bce5e4283c4a4654bca13e62d69f3
Jonas Praem <jonaspraem@gmail.com>
2018-10-22 19:56:06 +0200

    Add placeholder to announcement writer
    Also deleting unused directive

--- assets/app/@core/core.module.ts
+++ assets/app/@core/core.module.ts
@@ -1,15 +1,8 @@
 import { NgModule, Optional, SkipSelf } from "@angular/core";
 import { AuthenticationService } from "./services/authentication.service";
 import { AuthGuardService } from "./services/auth-guard.service";
-import { ContentEditableDirective } from "./directives/content-editable.directive";
 
 @NgModule({
-    declarations: [
-        ContentEditableDirective
-    ],
-    exports: [
-        ContentEditableDirective
-    ],
     providers: [
         AuthenticationService,
         AuthGuardService,
