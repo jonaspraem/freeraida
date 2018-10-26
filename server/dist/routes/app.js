3ab26e9f1d6dcc2699443d4d24d499c0c6739aa0
Jonas Praem <jonaspraem@gmail.com>
2018-10-26 22:16:14 +0200

    Delete dist folder

--- server/dist/routes/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-router.get('/', (req, res, next) => {
-    res.render('index');
-});
-module.exports = router;
-//# sourceMappingURL=app.js.map
