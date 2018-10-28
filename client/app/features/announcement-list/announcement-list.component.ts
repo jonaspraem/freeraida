43c0c629e64cd7516b9a585d7b46978ea4a5b662
Jonas Praem <jonaspraem@gmail.com>
2018-10-28 20:17:15 +0100

    Create announcement list component

---
+++ client/app/features/announcement-list/announcement-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-announcement-list',
+    templateUrl: './announcement-list.component.html'
+})
+
+export class AnnouncementListComponent {
+}
