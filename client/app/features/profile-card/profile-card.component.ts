2036ba885e7e5d4b93ffb9e3e4fd342b597e739d
Jonas Praem <jonaspraem@gmail.com>
2019-01-20 17:15:34 +0100

    Creating profile card

--- client/app/features/profile-card/profile-card.component.ts
+++ client/app/features/profile-card/profile-card.component.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
 
+const mockImage = require('../../../images/backgrounds/nevada-drawing.jpg');
+
 @Component({
     selector: 'app-profile-card',
-    templateUrl: './profile-card.html'
+    templateUrl: './profile-card.component.html'
 })
 
 export class ProfileCardComponent {
+    public mockImage = mockImage;
 
 }
--- client/app/pages/home-page/home.page.module.ts
+++ client/app/pages/home-page/home.page.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from "@angular/core";
 import { HomePageComponent } from "./home.page.component";
 import { PostWriterComponent } from "../../features/post-writer/post-writer.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { CommonModule } from "@angular/common";
+import { ProfileCardComponent } from "../../features/profile-card/profile-card.component";
 
 @NgModule({
     declarations: [
         HomePageComponent,
         PostWriterComponent,
+        ProfileCardComponent
     ],
     exports: [
         HomePageComponent
@@ -16,6 +19,7 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
     ],
     imports: [
         FontAwesomeModule,
+        CommonModule
     ]
 })
 
