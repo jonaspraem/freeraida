3a0a853ad2efc401e264b7be60ede69e7dac038a
Jonas Praem <jonaspraem@gmail.com>
2019-02-03 22:20:19 +0100

    Linking profile

--- client/app/components/header/user-nav-menu/user-nav-menu.component.ts
+++ client/app/components/header/user-nav-menu/user-nav-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { Router } from "@angular/router";
+import { IUserProfile } from "../../../models/interfaces/types";
 
 @Component({
     selector: 'app-user-nav-menu',
@@ -7,4 +8,5 @@ import { Router } from "@angular/router";
 })
 
 export class UserNavMenuComponent {
+    @Input() userProfile: IUserProfile;
 }
