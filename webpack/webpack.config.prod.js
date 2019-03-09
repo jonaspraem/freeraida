6d32d50bdffd77918dcc0822733bbfdfd672abf8
Jonas Praem <jonaspraem@gmail.com>
2019-03-09 15:54:59 +0100

    Small fix

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,4 +1,5 @@
-import { AngularCompilerPlugin } from '@ngtools/webpack';
+const AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
+const UglifyJsPlugin = require("uglifyjs-3-webpack-plugin");
 const path = require('path');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
@@ -19,22 +20,33 @@ module.exports = webpackMerge.smart(commonConfig, {
     module: {
         rules: [
             {
+                test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+                loader: '@ngtools/webpack'
+            },
+            {
                 test: /\.ts$/,
                 use: [
                     'awesome-typescript-loader',
                     'angular2-template-loader',
                     'angular-router-loader?aot=true'
-                ]
+                ],
+                exclude: [/node_modules/, /server/]
             }
         ]
     },
 
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                warnings: false,
+                ie8: false,
+                output: {
+                    comments: false
+                }
+            }
         }),
         new AngularCompilerPlugin({
-            tsConfigPath: './tsconfig.json',
+            tsConfigPath: './tsconfig.aot.json',
             entryModule: './assets/app/app.module#AppModule',
             sourceMap: true
         })
