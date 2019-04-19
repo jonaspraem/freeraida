57473f89b9130dbec843d975859fb63a3f0b4aee
Jonas Praem <jonaspraem@gmail.com>
2019-04-19 19:47:50 +0200

    Initial height map for line

--- client/app/pages/line-page/components/line-height-map/line-height-map.component.ts
+++ client/app/pages/line-page/components/line-height-map/line-height-map.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { ILine } from "../../../../models/interfaces/types";
+import { GoogleChartInterface } from "ng2-google-charts/google-charts-interfaces";
 
 @Component({
     selector: 'app-line-height-map',
@@ -6,5 +8,52 @@ import { Component } from "@angular/core";
 })
 
 export class LineHeightMapComponent {
-    
+    @Input() line: ILine;
+    public chart: GoogleChartInterface = {
+        chartType: 'AreaChart',
+        dataTable: null,
+        //opt_firstRowIsData: true,
+        options: {},
+    };
+
+    public ngOnInit(): void {
+        this.reMapChart();
+        this.chart.options = {
+            title: 'Height map',
+            legend: 'none',
+            vAxis: {
+                gridlines: {
+                    count: 0
+                },
+                baselineColor: 'none',
+            },
+            hAxis: {
+                gridlines: {
+                    count: 0
+                },
+                baselineColor: 'none'
+            },
+            axisFontSize: 0,
+            height: 400,
+            curveType: 'function',
+            pointSize: 0,
+            colors: ['#5E8894'],
+            backgroundColor: 'none',
+            chartArea: {
+                left: 0,
+                top: 0,
+                right: 0,
+                bottom: 0,
+            },
+        }
+    }
+
+    private reMapChart(): void {
+        const newData: any[] = [['X', 'Y']];
+        for(let i = 0; i < this.line.locations.length; i++) {
+            const location = this.line.locations[i];
+            newData.push([location.distanceFromStart, location.elevation]);
+        }
+        this.chart.dataTable = newData;
+    }
 }
--- client/app/pages/line-page/line.page.module.ts
+++ client/app/pages/line-page/line.page.module.ts
@@ -5,6 +5,7 @@ import { LineModule } from "../../components/line-module/line.module";
 import { LineOverviewComponent } from "./components/line-overview/line-overview.component";
 import { LineHeightMapComponent } from "./components/line-height-map/line-height-map.component";
 import { CommonModule } from "@angular/common";
+import { Ng2GoogleChartsModule } from "ng2-google-charts";
 
 @NgModule({
     declarations: [
@@ -21,7 +22,8 @@ import { CommonModule } from "@angular/common";
     imports: [
         lineRoutes,
         LineModule,
-        CommonModule
+        CommonModule,
+        Ng2GoogleChartsModule,
     ]
 })
 
