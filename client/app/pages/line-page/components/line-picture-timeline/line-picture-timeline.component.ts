b82e50474f1ced41281e2dd95739d221c5c24083
Jonas Praem <jonaspraem@gmail.com>
2019-05-11 17:22:24 +0200

    line image viewer transition

--- client/app/pages/line-page/components/line-picture-timeline/line-picture-timeline.component.ts
+++ client/app/pages/line-page/components/line-picture-timeline/line-picture-timeline.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
 import { ILine, ILineLocation } from "../../../../models/interfaces/types";
 
 @Component({
@@ -8,9 +8,11 @@ import { ILine, ILineLocation } from "../../../../models/interfaces/types";
 
 export class LinePictureTimelineComponent implements OnInit {
     @Input() line: ILine;
+    @ViewChild('image') imageElement: ElementRef;
     public imageAttachedLocations: ILineLocation[];
     public activeImageUrl: string;
     public currentIndex: number = 0;
+    public locationIndex: number = 0;
     private imageCount: number = 0;
 
     public ngOnInit(): void {
@@ -29,19 +31,35 @@ export class LinePictureTimelineComponent implements OnInit {
     private toggleImage(): void {
         if (this.currentIndex === this.imageCount) {
             this.currentIndex = 0;
+            this.locationIndex = 0;
         }
         let counter = 0;
         for(let i = 0; i < this.imageAttachedLocations.length; i++) {
             let location = this.imageAttachedLocations[i];
-            for (let image of location.images) {
+
+            for (let j = 0; j < location.images.length; j++) {
+                let image = location.images[j];
+                let isLast: boolean = !location.images[j + 1];
                 if (counter === this.currentIndex) {
-                    this.activeImageUrl = image;
-                    this.currentIndex++;
-                    return;
+                    this.imageElement.nativeElement.className = 'photo-timeline__image photo-timeline__image--fade-out';
+                    this.delay(1000).then(() => {
+                        this.activeImageUrl = image;
+                        this.currentIndex++;
+                        this.imageElement.nativeElement.className = 'photo-timeline__image';
+
+                        if (isLast) {
+                            this.locationIndex++;
+                        }
+                        console.log(isLast, this.locationIndex, this.currentIndex);
+                        return;
+                    });
                 }
                 counter++;
             }
         }
     }
 
+    async delay(ms: number) {
+        await new Promise(resolve => setTimeout(()=>resolve(), ms));
+    }
 }
