24736a640a4c63b05d7d6603b52900e6ab0ff409
Jonas Praëm <jpa@gmail.com>
2019-05-25 19:11:02 +0200

    Hide follow button if self

--- client/app/components/profile-module/profile-info-card/profile-info-card.component.ts
+++ client/app/components/profile-module/profile-info-card/profile-info-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from "@angular/core";
 import { IUserProfile } from "../../../models/interfaces/types";
 import { FLAG_DICTIONARY } from "../../../dictionary/flag-dictionary";
 import { SocialService } from "../../../core/services/social.service";
+import { ProfileService } from "../../../core/services/profile.service";
 
 @Component({
     selector: 'app-profile-info-card',
@@ -11,13 +12,16 @@ import { SocialService } from "../../../core/services/social.service";
 export class ProfileInfoCardComponent {
     @Input() userProfile: IUserProfile;
     @Input() isFollowing: boolean;
+    public isSelf: boolean;
 
     constructor(
         private _socialService: SocialService,
+        private _authService: ProfileService,
         public flagDictionary: FLAG_DICTIONARY,
     ) {}
 
     public onToggle(): void {
+        this._authService.userProfile$.subscribe(profile => this.isSelf = profile._id === this.userProfile._id);
         this.isFollowing ?
             this._socialService.unfollowUser(this.userProfile.username)
             :
