3dad113d5d2e14aa42ef3585957ea3d80ff82a0c
Jonas Praëm <jpa@gmail.com>
2019-06-11 15:55:50 +0200

    Line edit view

--- client/app/pages/line-page/components/line-overview/line-overview.component.ts
+++ client/app/pages/line-page/components/line-overview/line-overview.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit, Output } from "@angular/core";
 import { ILine } from "../../../../models/interfaces/types";
 import { COLOR_DICTIONARY } from "../../../../dictionary/color-dictionary";
+import { ProfileService } from "../../../../core/services/profile.service";
 
 @Component({
     selector: 'app-line-overview',
     templateUrl: './line-overview.component.html'
 })
 
-export class LineOverviewComponent {
+export class LineOverviewComponent implements OnInit {
     @Input() line: ILine;
+    public isOwn: boolean = false;
 
     constructor(
-        public colorDictionary: COLOR_DICTIONARY
+        public colorDictionary: COLOR_DICTIONARY,
+        public profileService: ProfileService
     ) {}
+
+    public ngOnInit(): void {
+        this.profileService.userProfile$.subscribe(profile => {
+            this.isOwn = this.line.username === profile.username;
+        });
+    }
 }
--- client/app/pages/line-page/line.page.component.ts
+++ client/app/pages/line-page/line.page.component.ts
@@ -10,6 +10,7 @@ import { ILine } from "../../models/interfaces/types";
 
 export class LinePageComponent implements OnInit {
     public line: ILine;
+    public hasImages: boolean = false;
 
     constructor(
         private _activatedRoute: ActivatedRoute,
@@ -19,7 +20,15 @@ export class LinePageComponent implements OnInit {
     public ngOnInit(): void {
         this._activatedRoute.params.subscribe(params => {
             const id = params['id'];
-            this._lineService.getLine(id).subscribe(line => this.line = line);
+            this._lineService.getLine(id).subscribe(line => {
+                this.line = line;
+                this.checkImages(line);
+            });
         });
+    }
+
+    private checkImages(line: ILine): void {
+        const imageAttachedLocations = this.line.locations.filter(loc => Array.isArray(loc.images));
+        this.hasImages = imageAttachedLocations.length > 0;
     }
 }
