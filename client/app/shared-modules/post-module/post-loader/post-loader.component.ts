7b0b1eedde9ec1e93e96a282c037e91079d1f230
Jonas Praem <jonaspraem@gmail.com>
2020-09-27 18:03:05 +0200

    post loader working

--- client/app/shared-modules/post-module/post-loader/post-loader.component.ts
+++ client/app/shared-modules/post-module/post-loader/post-loader.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { PostService } from '../../../core/services/post.service';
 
 @Component({
   selector: 'app-post-loader',
   templateUrl: './post-loader.component.html',
 })
-export class PostLoaderComponent {}
+export class PostLoaderComponent {
+  constructor(private postService: PostService) {}
+
+  public onClick(): void {
+    this.postService.getFeed();
+  }
+}
